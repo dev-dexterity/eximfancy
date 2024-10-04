31487f24f4bc09db2ea428d2864fe922b5b17448
Omkar Gaikwad <80238904+dev-dexterity@users.noreply.github.com>
2024-10-04 17:11:25 +0000

    making responsive

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-var swiper = new Swiper(".mySwiper", {
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+    spaceBetween: 100,
+ });
+}
+
+swiperAnimation()
+
+var menu = document.querySelector("nav h3")
+var full = document.querySelector("#full-scr")
+var navimg = document.querySelector("nav img")
+var flag = 0
+
+menu.addEventListener("click",function(){
+  if(flag == 0)
+  {
+    full.style.top = 0
+    navimg.style.opacity = 0
+    flag = 1
+  }
+  else {
+    full.style.top = "-100%"
+    navimg.style.opacity = 1
+    flag = 1
+  }
+})

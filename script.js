c8d5bfe7eeafeb02b0cad1df62cdc084d92f8e40
Omkar Gaikwad <80238904+dev-dexterity@users.noreply.github.com>
2024-10-04 08:09:31 +0000

    Made the Footer and added a demo slider

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
